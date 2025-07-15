8c47215082453315d31f740fabe5c14b996d30dc
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-07-15 12:15:21 +0100

    initialize Spring Boot REST project with PostgreSQL and JPA config

---
+++ src/main/java/com/maarketplace/MaarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.maarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaarketplaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maarketplace/MaarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaarketplaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
